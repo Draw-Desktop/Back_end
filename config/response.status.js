d733f997d2e587dde4a23a8ad7a8f43e0e2b0cb9
rlagns1234 <126846654+rlagns1234@users.noreply.github.com>
2024-02-01 00:10:20 +0900

    Feat: 기본 설정

---
+++ config/response.status.js
@@ -0,0 +1,6 @@
+const { StatusCodes } = require('http-status-codes');
+
+export const status = {
+    // success
+    SUCCESS: { isSuccess: true, code: 2000, message: 'success!' }
+};
---
+++ config/swagger.config.js
@@ -0,0 +1,16 @@
+const SwaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+    definition: {
+        info: {
+            title: 'DD API',
+            version: '1.0.0',
+            description: 'DD API with express, API 설명'
+        },
+        host: 'localhost:3000',
+        basepath: '../'
+    },
+    apis: ['./src/routes/*.js', './swagger/*']
+};
+
+export const specs = SwaggerJsdoc(options);
