8377a19715dd2860a9fb7fae26cf19d9c4951426
rlagns1234 <126846654+rlagns1234@users.noreply.github.com>
2024-02-09 22:07:15 +0900

    Feat: 도움말 기능 개발 완료

--- config/response.status.js
+++ config/response.status.js
@@ -2,5 +2,10 @@ const { StatusCodes } = require('http-status-codes');
 
 module.exports = {
     // success
-    SUCCESS: { isSuccess: true, code: 2000, message: 'success!' }
+    SUCCESS: { isSuccess: true, code: 2000, message: 'success!' },
+    SUCCESS_CREATE_HELP: { isSuccess: true, code: 2002, message: '요청이 성공적으로 접수되었습니다.' },
+
+    // create err
+    PARAMETER_IS_EMPTY: { status: StatusCodes.BAD_REQUEST, isSuccess: false, code: 'NOTICE4002', message: '필수 내용이 누락되었습니다.' },
+    CREATION_FAILED: { status: StatusCodes.BAD_REQUEST, isSuccess: false, code: 'NOTICE4003', message: '생성을 실패하였습니다.' },
 };
