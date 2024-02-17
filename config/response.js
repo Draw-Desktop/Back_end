c9c2e002d36e61b327a7d26763515e45a26b482e
hyen <hi020514@gmail.com>
2024-02-17 23:30:29 +0900

    fix:오류수정

--- config/response.js
+++ config/response.js
@@ -1,17 +1,27 @@
-const categoryService = require('./category.service');
-const { response, errResponse } = require('../../config/response');
-
-// 전체 카테고리 가져오기
-exports.getAllCategories = async (req, res, next) => {
-    try {
-        // 카테고리 서비스를 호출하여 전체 카테고리를 가져옵니다.
-        const categories = await categoryService.getAllCategories();
-        
-        // 성공적으로 카테고리를 가져온 경우 응답을 보냅니다.
-        res.status(200).json(response({ isSuccess: true, code: 200, message: "전체 카테고리를 성공적으로 불러왔습니다.", result: categories }));
-    } catch (error) {
-        // 오류가 발생한 경우 오류 응답을 보냅니다.
-        console.error('Error fetching categories:', error);
-        res.status(500).json(errResponse({ isSuccess: false, code: 500, message: "카테고리를 불러오는 데 실패했습니다." }));
-    }
+const response = ({ isSuccess, code, message }, result) => {
+    return {
+        isSuccess: isSuccess,
+        code: code,
+        message: message,
+        result: result,
+    };
 };
+
+const errResponse = ({ isSuccess, code, message }) => {
+    return {
+        isSuccess: isSuccess,
+        code: code,
+        message: message,
+    };
+};
+
+const getSuccessSignInJson = (userId, aToken) => {
+    return {
+        status_code: 200,
+        message: '로그인에 성공했습니다.',
+        user_id: userId,
+        access_token: aToken
+    };
+};
+
+module.exports = { response, errResponse, getSuccessSignInJson };
--- src/category/category.controller.js
+++ src/category/category.controller.js
@@ -1,11 +1,11 @@
-const categoryService = require('./category.service');
+const categoryProvider = require('./category.provider');
 const { errResponse } = require('../../config/response');
 
 // 전체 카테고리 가져오기
-exports.getAllCategories = async (req, res, next) => {
+module.exports.getAllCategories = async (req, res, next) => {
     try {
         // 카테고리 서비스를 호출하여 전체 카테고리를 가져옵니다.
-        const categories = await categoryService.getAllCategories();
+        const categories = await categoryProvider.getAllCategories();
         
         // 성공적으로 카테고리를 가져온 경우 응답을 보냅니다.
         res.status(200).json({
