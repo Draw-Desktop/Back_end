1ce90c7a97d1e9717e6787c8c19cf0c71cf60e06
rlagns1234 <126846654+rlagns1234@users.noreply.github.com>
2024-02-18 14:13:41 +0900

    Add files via upload

--- src/popular/popular.dto.js
+++ src/popular/popular.dto.js
@@ -1,32 +1,48 @@
-const Category = require('../../models/category');
-const Post = require('../../models/post');
+const Post = require('../../models/post.js');
+const Category = require('../../models/category.js');
+const likePost = require('../../models/like_post.js');
+const likeDto = require('../like/like.dto.js');
 
+exports.getPopularWallpaperResponseDTO = async () => {
+    const EX_POST = await Post.findAll();
 
-exports.getAllCategories = async () => {
-    const categories = await Category.findAll();
+    const EX_POST_LIKE = await Promise.all(EX_POST.map(async (post) => {
+        const like = await likePost.count({
+            where: {
+                post_id: post.post_id
+            }
+        });
+    
+        post.dataValues.like = like;
+        
+        return post;
+    }));
+    
+    // EX_POST_LIKE를 post.like 기준으로 내림차순으로 정렬
+    EX_POST_LIKE.sort((a, b) => b.dataValues.like - a.dataValues.like);
 
-    const EX_categories = await Promise.all(categories.map(async (category) => {
+    if (EX_POST_LIKE === null) return false;
+    else return EX_POST_LIKE;
+};
+
+exports.getPopularCategoryResponseDTO = async () => {
+    const EX_CATEGORY = await Category.findAll();
+
+    const EX_POST_CATEGORY = await Promise.all(EX_CATEGORY.map(async (category) => {
         const post = await Post.findAll({
             where: {
                 category_id: category.category_id
             }
         });
-
-        // const recent = await Post.findAll({
-        //     where: {
-        //         category_id: category.category_id
-
-        //     }
-        // });
-
+    
         category.dataValues.post = post;
         category.dataValues.count = post.length;
         return category;
     }));
-
+    
     // EX_POST_LIKE를 post.like 기준으로 내림차순으로 정렬
-    EX_categories.sort((a, b) => b.dataValues.count - a.dataValues.count);
+    EX_POST_CATEGORY.sort((a, b) => b.dataValues.count - a.dataValues.count);
 
-    if (EX_categories === null) return false;
-    else return EX_categories;
+    if (EX_POST_CATEGORY === null) return false;
+    else return EX_POST_CATEGORY;
 };
